89ece7a152901951d004b8837f6fa723fc7b9bfa
York9715 <york9715@gmail.com>
2016-08-13 18:39:01 -0400

    update2
    Mark upate2

--- src/main/java/com/dbl/config/AppConfig.java
+++ src/main/java/com/dbl/config/AppConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
-//Annotation configuration
+//Annotation configuration2
 
 @EnableWebMvc
 @Configuration
