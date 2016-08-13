ed04ce8846e3b0314df40e594d616e3093153a04
york.chen <york.tips@gmail.com>
2016-08-13 18:28:27 -0400

    added node 1

--- src/main/java/com/dbl/config/AppConfig.java
+++ src/main/java/com/dbl/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.dbl.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,8 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+//Annotation configuration
 
 @EnableWebMvc
 @Configuration
